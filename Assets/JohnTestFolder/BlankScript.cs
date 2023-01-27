469e83d45f23e6d7c576fb4f3729315ae5a55355
JohnColleraUCF <johncollera02@knights.ucf.edu>
2023-01-27 01:49:07 -0500

    Commit test

--- Assets/JohnTestFolder/BlankScript.cs
+++ Assets/JohnTestFolder/BlankScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fingersCrossed : MonoBehaviour
+public class BlankScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
